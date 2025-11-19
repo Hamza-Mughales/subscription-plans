39c95624db86bb97ba307f68f85bee67ce5a35e1
Hamza Mughales <hamzawemughales@gmail.com>
2025-11-19 22:04:49 +0300

    feat: payment method type enum

---
+++ src/Enums/PaymentMethodType.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NootPro\SubscriptionPlans\Enums;
+
+use Filament\Support\Contracts\HasLabel;
+
+enum PaymentMethodType: string implements HasLabel
+{
+    case BankTransfer  = 'bank_transfer';
+    case OnlinePayment = 'online_payment';
+    case Visa          = 'visa';
+
+    /**
+     * Get all payment method type values as array.
+     *
+     * @return array<int, string>
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    /**
+     * Get the display label for the payment method type.
+     * Compatible with Filament's HasLabel interface.
+     */
+    public function getLabel(): string
+    {
+        return __('subscription-plans::subscription-plans.payment-method-type.'.$this->value);
+    }
+}
