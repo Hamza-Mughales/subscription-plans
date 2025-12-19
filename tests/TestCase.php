91074c8555df5885ec759ee1921f37e98110f9a7
Hamza Mughales <hamzawemughales@gmail.com>
2025-12-19 10:54:46 +0300

    fix: composer load migrations files

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,27 +26,13 @@ class TestCase extends Orchestra
         ];
     }
 
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+
     public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
-
-        // Load migrations
-        $migration = include __DIR__.'/../database/migrations/2022_03_05_200700_create_plans_table.php';
-        $migration->up();
-
-        $migration = include __DIR__.'/../database/migrations/2022_03_05_200701_create_plan_features_table.php';
-        $migration->up();
-
-        $migration = include __DIR__.'/../database/migrations/2022_03_05_200702_create_plan_subscriptions_table.php';
-        $migration->up();
-
-        $migration = include __DIR__.'/../database/migrations/2022_03_05_200703_create_plan_subscription_usage_table.php';
-        $migration->up();
-
-        $migration = include __DIR__.'/../database/migrations/2022_03_05_200704_create_plan_subscription_features_table.php';
-        $migration->up();
-
-        $migration = include __DIR__.'/../database/migrations/2025_08_05_105105_create_plan_modules_table.php';
-        $migration->up();
     }
 }
